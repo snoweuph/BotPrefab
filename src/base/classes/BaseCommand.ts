17a27584c3ee04e9ee65c93ddb98eb2b8017d8b5
Snoweuph <domsaeume@gmail.com>
2022-02-05 13:45:16 +0100

    structure

---
+++ src/base/classes/BaseCommand.ts
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { Client, MessageComponentInteraction, Options } from "discord.js";
+
+export default abstract class BaseCommand {
+    data: SlashCommandBuilder;
+    category: string;
+
+    constructor(data: SlashCommandBuilder, category: string) {
+        this.data = data;
+        this.category = category;
+    }
+
+    abstract execute(client: Client<boolean>, message: MessageComponentInteraction, options: Options): Promise<void>;
+}
---
+++ src/base/classes/BaseEvent.ts
@@ -0,0 +1,11 @@
+import { Client, ClientEvents } from "discord.js";
+
+export default abstract class BaseEvent {
+    event: keyof ClientEvents;
+
+    constructor(event: keyof ClientEvents) {
+        this.event = event;
+    }
+
+    abstract execute(client: Client<boolean>, ...args: any[]): Promise<void>;
+}
