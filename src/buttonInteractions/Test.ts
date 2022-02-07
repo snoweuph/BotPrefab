fdd07770d8e7b62d45fe1cfcd825fd1d16269a8f
Snoweuph <domsaeume@gmail.com>
2022-02-07 15:08:14 +0100

    updated test button interaction

--- src/buttonInteractions/Test.ts
+++ src/buttonInteractions/Test.ts
@@ -1,5 +1,6 @@
 import { ButtonInteraction, CacheType, MessageActionRow } from 'discord.js';
 import BaseButtonInteraction from '../base/classes/BaseButtonInteraction';
+import { run } from '../modules/PingModule';
 import Client from '../types/Client';
 
 export default class TestButtonInteraction extends BaseButtonInteraction {
@@ -8,6 +9,6 @@ export default class TestButtonInteraction extends BaseButtonInteraction {
     }
 
     async execute(client: Client, interaction: ButtonInteraction<CacheType>): Promise<void> {
-        interaction.reply('TestButtonInteraction');
+        await run(client.client, interaction);
     }
 }
