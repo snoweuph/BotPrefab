ff86d992747afc736a1d318bfdc9e8cce37315af
Snoweuph <domsaeume@gmail.com>
2022-02-22 01:29:55 +0100

    settup auth: need to handle stupid users still (wrong code)

--- src/dashboard/strategies/Discord.ts
+++ src/dashboard/strategies/Discord.ts
@@ -1,6 +1,21 @@
 import passport from 'passport';
 import { Profile, Strategy } from 'passport-discord';
 import { VerifyCallback } from 'passport-oauth2';
+import { User } from '../../base/schemas/Index';
+
+passport.serializeUser((user: any, done) => {
+    return done(null, user.id);
+})
+
+passport.deserializeUser(async (id: string, done) => {
+    try {
+        const user = await User.findById(id);
+        return user ? done(null, user) : done(null, null);
+    } catch (error) {
+        console.log(error);
+        return done(error, null);
+    }
+});
 
 passport.use(new Strategy({
     clientID: process.env.CLIENT_ID,
@@ -8,5 +23,19 @@ passport.use(new Strategy({
     callbackURL: process.env.DASHBOARD_CALLBACK_URI,
     scope: ['identify', 'guilds'],
 }, async (accesToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
+    const { id: discordId } = profile;
+    try {
+        const existingUser = await User.findOneAndUpdate(
+            { discordId },
+            { accesToken, refreshToken },
+            { new: true }
+        );
+        if (existingUser) return done(null, existingUser);
+        const newUser = await new User({ discordId, accesToken, refreshToken }).save();
+        return done(null, newUser);
+    } catch (error) {
+        console.log(error);
+        return done(error as any, undefined)
+    }
 
 }));
