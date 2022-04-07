fd7ead4cdb8030e1100d24a86cc3afede0911e61
Snoweuph <domsaeume@gmail.com>
2022-04-07 11:54:28 +0200

    cleanup

--- src/bot/src/sharded.ts
+++ src/bot/src/sharded.ts
@@ -1,17 +1,14 @@
-import { Shard } from 'discord.js';
+import discord, { Shard } from 'discord.js';
 
 /* eslint-disable-next-line */
 require('dotenv').config();
 /* eslint-disable-next-line */
-const DiscordJS = require('discord.js');
 
-const manager = new DiscordJS.ShardingManager('./bot.ts', {
+const manager = new discord.ShardingManager('./src/bot.ts', {
 	token: process.env.DISCORD_BOT_TOKEN,
-
 });
 
 manager.on('shardCreate', (shard: Shard) => {
 	console.log(`Launched shard ${shard.id}`);
 })
-
 manager.spawn();
