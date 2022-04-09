0f7520aacdbd4690f11bed0cd3caf8a11aea1002
Snoweuph <domsaeume@gmail.com>
2022-04-09 06:09:57 +0200

    + dev env; + guildSpecific Commands

--- src/bot/src/base/classes/baseCommand.ts
+++ src/bot/src/base/classes/baseCommand.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from '@discordjs/builders';
-import { CacheType, CommandInteraction, CommandInteractionOption, PermissionResolvable } from 'discord.js';
+import { CacheType, CommandInteraction, CommandInteractionOption, GuildResolvable, PermissionResolvable } from 'discord.js';
 import Bot from '@baseTypes/bot';
 import BaseCommandCategory from '@baseTypes/baseCommandCategory';
 
@@ -8,11 +8,12 @@ export default abstract class BaseCommand {
 	category: BaseCommandCategory;
 	cooldown: number;
 	permissions: Array<PermissionResolvable>;
-
-	constructor(data: SlashCommandBuilder | Omit<SlashCommandBuilder, 'addSubcommandGroup' | 'addSubcommand'> | SlashCommandSubcommandsOnlyBuilder, cooldown?: number, permissions?: Array<PermissionResolvable>) {
+	guilds: Array<GuildResolvable>;
+	constructor(data: SlashCommandBuilder | Omit<SlashCommandBuilder, 'addSubcommandGroup' | 'addSubcommand'> | SlashCommandSubcommandsOnlyBuilder, cooldown?: number, permissions?: Array<PermissionResolvable>, guilds?: Array<GuildResolvable>) {
 		this.data = data;
 		this.cooldown = cooldown || 0;
 		this.permissions = permissions || [];
+		this.guilds = guilds || [];
 	}
 
 	abstract execute(bot: Bot, interaction: CommandInteraction<CacheType>, options: Readonly<Array<CommandInteractionOption<CacheType>>>): Promise<void>;
