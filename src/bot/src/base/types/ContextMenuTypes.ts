343faf9f3f1ecf84f063f59ba64b6752f3f931ce
Snoweuph <domsaeume@gmail.com>
2022-04-09 14:49:46 +0200

    added tsdoc to all custom types

--- src/bot/src/base/types/ContextMenuTypes.ts
+++
@@ -1,6 +0,0 @@
-enum ContextMenuTypes {
-    USER,
-    MESSAGE,
-}
-
-export default ContextMenuTypes;
--- src/bot/src/base/types/embdTypes.ts
+++ src/bot/src/base/types/embdTypes.ts
@@ -1,3 +1,8 @@
+/* eslint-disable-next-line */
+import Embds from "@base/utils/embds";
+/**
+ * All the embed types for the Utils {@link Embds} class.
+ */
 enum EmbdTypes {
     NORMAL = '#248EA4',
     SUCCESS = '#0fbf0f',
